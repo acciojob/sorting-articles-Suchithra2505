5f9416d83081b316b20eba126c7af0e6361d7150
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2023-12-26 18:49:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bandss = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
     // Remove specified words from each band name and sort the bands
-    const sortedBands = bands.map(band => band.replace(/^(a |an |the )/i, '').trim()).sort();
+    const sortedBands = bandss.map(band => band.replace(/^(a |an |the )/i, '').trim()).sort();
 
     // Display sorted bands in the unordered list
     const bandList = document.getElementById('band');
